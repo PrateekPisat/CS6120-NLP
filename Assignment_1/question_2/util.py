6db2406fdf4dc1ded601c2fb7b71c8cf9646941a
Prateek Pisat <pisatprateek12@gmail.com>
2019-10-09 17:25:01 -0400

    Fix git stupidity.

--- Assignment_1/question_2/util.py
+++ Assignment_1/question_2/util.py
@@ -1,8 +1,7 @@
 import os
 
+import pandas as pd
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
 
 
 def report_to_file(vocab, header=None):
@@ -19,15 +18,20 @@ def _get_head_word_index(word_list, head_word, start_index):
 
 
 def get_top_features_for_instance(X, y, all_features):
-    k_best = SelectKBest(score_func=chi2, k=10)
-    # fit on train set
-    k_best.fit(X, y)
 
-    # model = LogisticRegression()
-    # rfe = RFE(model, 10)
-    # fit = rfe.fit(X, y)
+    bestfeatures = SelectKBest(score_func=chi2, k=10)
+    fit = bestfeatures.fit(X, y)
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(all_features)
+    # concat two dataframes for better visualization
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+    print(featureScores.nlargest(10, 'Score'))  # print 10 best features
 
-    feature_indices = k_best.get_support(indices=True)
+    # k_best = SelectKBest(score_func=chi2, k=10)
+    # # fit on train set
+    # k_best.fit(X, y)
+    # feature_indices = k_best.get_support(indices=True)
 
     return list(all_features[feature_indices])
 
--- Assignment_1/question_3/perplexity.py
+++ Assignment_1/question_3/perplexity.py
@@ -1,3 +1,5 @@
+import os
+
 from nltk.tokenize import sent_tokenize
 
 from language_model import get_add_alpha_smoothing_model, get_interpolation_model
@@ -35,22 +37,30 @@ def get_perplexity_interpolation(model, test_files):
 
 
 if __name__ == "__main__":
+    train_dir = ".{sep}{dir}{sep}".format(sep=os.sep, dir="train")
+    test_dir = ".{sep}{dir}{sep}".format(sep=os.sep, dir="test")
     training_files = [
-        "./train/5d384641-37e5-4b1c-b4d6-0ee935141ecb.txt",
-        "./train/9ae9dae7-69a0-4116-9622-448f154bc269.txt",
-        "./train/14d44997-7510-4777-adf0-5e4dd387e0bf.txt",
-        "./train/0493e223-a0e2-4c6f-ade9-7172f35c18b1.txt",
-        "./train/521ce35b-288c-4b12-a7a8-70b836290f90.txt",
-        "./train/904393ad-fbc1-4512-8705-ce1c005c4915.txt",
-        "./train/30381986-3d6b-4227-9733-9483ead7343d.txt",
-        "./train/a894e49e-a0a6-4851-be23-4da89a52bb8e.txt",
-        "./train/da059d4f-19ac-4130-858a-f6241b56fe39.txt",
-        "./train/e0a13927-26e0-4ca8-b1a0-864b7604e566.txt",
+        "{train_dir}5d384641-37e5-4b1c-b4d6-0ee935141ecb.txt".format(train_dir=train_dir),
+        "{train_dir}9ae9dae7-69a0-4116-9622-448f154bc269.txt".format(train_dir=train_dir),
+        "{train_dir}14d44997-7510-4777-adf0-5e4dd387e0bf.txt".format(train_dir=train_dir),
+        "{train_dir}0493e223-a0e2-4c6f-ade9-7172f35c18b1.txt".format(train_dir=train_dir),
+        "{train_dir}521ce35b-288c-4b12-a7a8-70b836290f90.txt".format(train_dir=train_dir),
+        "{train_dir}904393ad-fbc1-4512-8705-ce1c005c4915.txt".format(train_dir=train_dir),
+        "{train_dir}30381986-3d6b-4227-9733-9483ead7343d.txt".format(train_dir=train_dir),
+        "{train_dir}a894e49e-a0a6-4851-be23-4da89a52bb8e.txt".format(train_dir=train_dir),
+        "{train_dir}da059d4f-19ac-4130-858a-f6241b56fe39.txt".format(train_dir=train_dir),
+        "{train_dir}e0a13927-26e0-4ca8-b1a0-864b7604e566.txt".format(train_dir=train_dir),
     ]
-    test_files = ["./test/test01.txt", "./test/test02.txt"]
-    # model = get_add_alpha_smoothing_model(training_files, 0.1)
-    # di = get_perplexity(model, test_files)
-    # print(di)
+    test_files = [
+        "{test_dir}test01.txt".format(test_dir=test_dir),
+        "{test_dir}test02.txt".format(test_dir=test_dir)
+    ]
+    model = get_add_alpha_smoothing_model(training_files, 0.1)
+    di = get_perplexity(model, test_files)
+    print("lambda = 0.1 " + str(di))
+    model = get_add_alpha_smoothing_model(training_files, 0.3)
+    di = get_perplexity(model, test_files)
+    print("lambda = 0.3 " + str(di))
     model = get_interpolation_model(training_files[:-2], training_files[-2:])
     di = get_perplexity_interpolation(model, test_files)
-    print(di)
+    print("Interpolation " + str(di))
